a7dd9aa090d96165d9234695a6017f0cd086c5e6
shimyoungmin <dudals6965@gmail.com>
2021-07-06 11:30:25 +0900

    Add MostBigNumber

--- Programmers/Programmers/42746_MostBigNumber.swift
+++ Programmers/Programmers/42746_MostBigNumber.swift
@@ -6,3 +6,11 @@
 //
 
 import Foundation
+
+func solution3(_ numbers: [Int]) -> String {
+    let sorted: [Int] = numbers.sorted{ Int("\($0)\($1)")! > Int("\($1)\($0)")! }
+    if sorted[0] == 0 {
+        return "0"
+    }
+    return sorted.reduce(""){ $0+"\($1)" }
+}
