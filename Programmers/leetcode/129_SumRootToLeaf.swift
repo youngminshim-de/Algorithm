d877897f5abbcbf95e2a42aa35d240b9914ba10c
shimyoungmin <dudals6965@gmail.com>
2021-07-23 12:38:02 +0900

    Modify leetcode SumRootToLeaf

--- Programmers/leetcode/129_SumRootToLeaf.swift
+++ Programmers/leetcode/129_SumRootToLeaf.swift
@@ -28,12 +28,40 @@ class TreeNode {
         self.left = left
         self.right = right
     }
-    
 }
 
 class SumRootToLeaf {
     static func sumNumbers(_ root: TreeNode?) -> Int {
+        guard let root = root else {
+            return 0
+        }
+        var stack = [(root, 0)]
+        var result = 0
         
-        return 0
+        while !stack.isEmpty {
+            let (node, val) = stack.popLast()!
+            // 하위 노드로 내려갈때 마다 10씩 곱해주고 val을 더해준다.
+            // 1 -> 2 -> 3
+            // 0 * 10 + 1 = 1
+            // 1 * 10 + 2 = 12
+            // 12 * 10 + 3 = 123
+            let sumVal = val * 10 + node.val
+            
+            // 마지막 노드라면 result값에 sumVal를 더한다.
+            if node.left == nil && node.right == nil {
+                result += sumVal
+            } else {
+                // 왼쪽 노드를 stack에 추가해준다.
+                if let left = node.left {
+                    stack.append((left,sumVal))
+                }
+                // 오른쪽 노드 또한 stack에 추가해준다.
+                // 같은 sumVal를 넘겨줌으로써 같은 level로 만들어준다.
+                if let right = node.right {
+                    stack.append((right,sumVal))
+                }
+            }
+        }
+        return result
     }
 }
--- Programmers/main.swift
+++ Programmers/main.swift
@@ -11,3 +11,6 @@ print("solution1 : \(HIndex.solution([3,0,6,1,5]))")
 print("solution1 : \(HIndex.solution([10, 100]))")
 print("solution1 : \(HIndex.solution([2,2,2]))")
 print("solution1 : \(HIndex.solution([6,6,6,6,6,6]))")
+
+var root = TreeNode(1, TreeNode(2, TreeNode(4), TreeNode(5)), TreeNode(3, TreeNode(6), nil))
+print(SumRootToLeaf.sumNumbers(root))
