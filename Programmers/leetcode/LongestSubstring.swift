2a9fe8837b7bb7b8b7505868e77975b72011020c
shimyoungmin <dudals6965@gmail.com>
2021-07-31 17:08:40 +0900

    Add leetcode LongestSubstring Solution#2

--- Programmers/leetcode/LongestSubstring.swift
+++ Programmers/leetcode/LongestSubstring.swift
@@ -8,22 +8,36 @@
 import Foundation
 
 class LongestSubString {
+//    static func lengthOfLongestSubstring(_ s: String) -> Int {
+//        let str = s.map{$0}
+//        var result = 0
+//
+//        var temp = ""
+//        for i in 0..<str.count {
+//            for j in i..<str.count {
+//                if temp.contains(str[j]) {
+//                    break
+//                } else {
+//                    temp += String(str[j])
+//                }
+//            }
+//            result = max(result, temp.count)
+//            temp = ""
+//        }
+//        return result
+//    }
+    
     static func lengthOfLongestSubstring(_ s: String) -> Int {
-        let str = s.map{$0}
-        var result = 0
+        var length = 0
+        var result = [String]()
         
-        var temp = ""
-        for i in 0..<str.count {
-            for j in i..<str.count {
-                if temp.contains(str[j]) {
-                    break
-                } else {
-                    temp += String(str[j])
-                }
+        s.forEach { char in
+            if result.contains(String(char)) {
+                result.removeSubrange(0...result.firstIndex(of: String(char))!)
             }
-            result = max(result, temp.count)
-            temp = ""
+            result.append(String(char))
+            length = max(length, result.count)
         }
-        return result
+        return length
     }
 }
