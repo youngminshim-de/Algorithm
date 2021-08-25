cbe17f0d3099af7b7b57c54d445a8057b5f5fe21
shimyoungmin <dudals6965@gmail.com>
2021-08-25 15:41:06 +0900

    Modify leetcode Rabbits In Forest

--- Programmers/leetcode/781_Rabbits.swift
+++ Programmers/leetcode/781_Rabbits.swift
@@ -19,11 +19,13 @@ class Rabbits {
             let quotient = value / (key + 1)
             let remainder = value % (key + 1)
             
-            if quotient != 0 && remainder == 0 {
-                result += quotient * (key + 1)
-            } else {
-                result += (quotient + 1) * (key + 1)
-            }
+//            if quotient != 0 && remainder == 0 {
+//                result += quotient * (key + 1)
+//            } else {
+//                result += (quotient + 1) * (key + 1)
+//            }
+            
+            result += remainder == 0 ? quotient * (key + 1) : (quotient + 1) * (key + 1)
         }
         
         return result
